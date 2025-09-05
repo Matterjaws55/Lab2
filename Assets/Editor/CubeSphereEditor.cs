fedfa7550a6ab1959b1ef19307d41585984d99c0
Matterjaws55 <163688327+Matterjaws55@users.noreply.github.com>
2025-09-05 00:05:23 -0400

    Added features

--- Assets/Editor/CubeSphereEditor.cs
+++ Assets/Editor/CubeSphereEditor.cs
@@ -1,17 +1,49 @@
 using UnityEngine;
-using System.Collections;
 using UnityEditor;
 
 [CustomEditor(typeof(CubeSphere))]
 public class CubeSphereEditor : Editor
 {
+    private bool cubeButtonPressed = false;
+    private bool sphereButtonPressed = false;
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
         CubeSphere cubeSphere = (CubeSphere)target;
-        if(GUILayout.Button("Hide Cube"))
+
+        if(cubeSphere.size >= 1.999999999999f) 
+            EditorGUILayout.HelpBox("Objects can not be larger than 1.999999999999", MessageType.Warning);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Select all cubes"))
         {
-            cubeSphere.HideCube();
+            cubeSphere.SelectCubes();
         }
+        if (GUILayout.Button("Select all spheres"))
+        {
+            cubeSphere.SelectSpheres();
+        }
+        GUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Clear selection"))
+        {
+            cubeSphere.ClearSelection();
+        }
+
+        GUILayout.BeginHorizontal();
+        GUI.backgroundColor = cubeButtonPressed ? new Color(0.5f, 0f, 0.5f) : Color.blue;
+        if (GUILayout.Button("Disable/Enable cubes"))
+        {
+            cubeButtonPressed = !cubeButtonPressed;
+            cubeSphere.EnableDisableCubes();
+        }
+
+        GUI.backgroundColor = sphereButtonPressed ? new Color(0.5f, 0f, 0.5f) : Color.blue;
+        if (GUILayout.Button("Disable/Enable spheres"))
+        {
+            sphereButtonPressed = !sphereButtonPressed;
+            cubeSphere.EnableDisableSpheres();
+        }
+        GUILayout.EndHorizontal();
     }
 }
--- Assets/Scripts/CubeSphere.cs
+++ Assets/Scripts/CubeSphere.cs
@@ -1,14 +1,57 @@
 using UnityEngine;
-using System.Collections;
+using UnityEditor;
 
 public class CubeSphere : MonoBehaviour
 {
-    public int size;
-    public GameObject cube;
-    public GameObject sphere;
+    public float size = 1f;
+    public GameObject[] cubes;
+    public GameObject[] spheres;
 
-    public void HideCube()
+    private bool cubesDisabled = false;
+    private bool spheresDisabled = false;
+
+    private void OnValidate()
     {
+        foreach (GameObject cube in cubes)
+        {
+            cube.transform.localScale = Vector3.one * size;
+        }
+        foreach (GameObject sphere in spheres)
+        {
+            sphere.transform.localScale = Vector3.one * size;
+        }
+    }
 
+    public void SelectCubes()
+    {
+        Selection.objects = cubes;
+    }
+    public void SelectSpheres()
+    {
+        Selection.objects = spheres;
+    }
+
+    public void ClearSelection()
+    {
+        Selection.objects = new Object[0];
+    }
+
+    public void EnableDisableCubes()
+    {
+        cubesDisabled = !cubesDisabled;
+
+        foreach (GameObject cube in cubes)
+        {
+            cube.SetActive(!cubesDisabled);
+        }
+    }
+    public void EnableDisableSpheres()
+    {
+        spheresDisabled = !spheresDisabled;
+
+        foreach (GameObject sphere in spheres)
+        {
+            sphere.SetActive(!spheresDisabled);
+        }
     }
 }
